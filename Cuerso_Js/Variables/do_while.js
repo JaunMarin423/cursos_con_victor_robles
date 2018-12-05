bf9362024e6b3f7f50c6f76046e9af49f9b32da1
JuanMarin <wvany1996@gmail.com>
2018-12-05 15:22:52 -0500

    Repaso de bucles

---
+++ Cuerso_Js/Variables/do_while.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Do while
+
+var year = 30 ;
+
+do {
+  alert("Solo cuando sea diferente a 20")
+  year--;
+} while (year > 25);
---
+++ Cuerso_Js/Variables/while.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Bucle while
+
+var year = 2018;
+
+while (year != 1991) {
+  //ejecuta esto
+
+  console.log("Estamos en el a;o" + year);
+
+  year--;
+}

08906b0b47fde0eb40043962612a71a91d418e50
JuanMarin <wvany1996@gmail.com>
2018-12-19 15:33:45 -0500

    try-carch

---
+++ Cuerso_Js/Json/try-catch.js
@@ -0,0 +1,12 @@
+'use strict'
+try {
+
+    var year = 2019;
+
+    alert(year);
+    console.log(decodeURIComponent("https://google.com"));
+    
+} catch (error) {
+    console.log(error); 
+    
+}

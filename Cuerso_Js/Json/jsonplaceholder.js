8daa0033344daad2de87444faaa29bf1ed2158f0
JuanMarin <wvany1996@gmail.com>
2018-12-19 12:30:25 -0500

    promesas y fetch

--- Cuerso_Js/Json/jsonplaceholder.js
+++ Cuerso_Js/Json/jsonplaceholder.js
@@ -4,25 +4,55 @@
 
 
 var div_usuarios = document.querySelector("#usuarios");
+var div_janet = document.querySelector("#janet");
 
-var usuarios = [];
 
-fetch('https://reqres.in/api/users?page=2')
+
+
 //capturar por medio de then que es la promesa acapturar los datos
+    getUsuarios()
     .then(data => data.json())
     //recoger datos
     .then(users => {
         //recoger datos
-        usuarios = users.data;
-        console.log(usuarios);
-
-        usuarios.map((user, i) => {
-                let nombre = document.createElement('h3');
-
-                nombre.innerHTML = i + '. ' + user.first_name + " " + user.last_name;
-
-                div_usuarios.appendChild(nombre);
-                document.querySelector(".loading").style.display = 'none';
-        });
+        listadoUsuaros(users.data);
         
+        return getJanet();
+    })
+    .then(data => data.json())
+    .then(user => {
+            mostrarJanet(user.data);
     });
+
+    function getUsuarios() {
+        return fetch('https://reqres.in/api/users?page=2');
+    }
+
+    function getJanet() {
+        return fetch('https://reqres.in/api/users/2');
+    }
+
+    function listadoUsuaros (usuarios){
+        usuarios.map((user, i) => {
+            let nombre = document.createElement('h3');
+
+            nombre.innerHTML = i + '. ' + user.first_name + " " + user.last_name;
+
+            div_usuarios.appendChild(nombre);
+            document.querySelector(".loading").style.display = 'none';
+        });
+    }
+//Obgetos limpios no necesito usar metodo MAP!
+    function mostrarJanet(user){      
+        let nombre = document.createElement('h4');
+        let avatar = document.createElement('img');
+        nombre.innerHTML = user.first_name + " " + user.last_name;
+        avatar.src = user.avatar;
+        
+        avatar.width = '100';
+
+        div_janet.appendChild(nombre);
+        div_janet.appendChild(avatar);
+
+            document.querySelector("#janet .loading").style.display = 'none';
+    }

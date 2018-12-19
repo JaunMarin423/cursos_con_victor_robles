92aa0c861131d9f7dc782a8c09db4a088cc233d5
JuanMarin <wvany1996@gmail.com>
2018-12-19 10:59:46 -0500

    se arregla linea 19 y 22

--- Cuerso_Js/Json/jsonplaceholder.js
+++ Cuerso_Js/Json/jsonplaceholder.js
@@ -7,7 +7,7 @@ var div_usuarios = document.querySelector("#usuarios");
 
 var usuarios = [];
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://reqres.in/api/users?page=2')
 //capturar por medio de then que es la promesa acapturar los datos
     .then(data => data.json())
     //recoger datos
@@ -16,10 +16,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
         usuarios = users.data;
         console.log(usuarios);
 
-        usuarios.map((users, i) => {
+        usuarios.map((user, i) => {
                 let nombre = document.createElement('h3');
 
-                nombre.innerHTML = i + '. ' + user.first_name +" " + user.last_name;
+                nombre.innerHTML = i + '. ' + user.first_name + " " + user.last_name;
 
                 div_usuarios.appendChild(nombre);
                 document.querySelector("#loading").style.display = 'none';

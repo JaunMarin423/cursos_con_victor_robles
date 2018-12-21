a8cecb2cd2c0d965e605659da03ea6f053139480
JuanMarin <wvany1996@gmail.com>
2018-12-20 22:11:24 -0500

    selector de etiqueta

--- Cuerso_Js/AprediendoJquery/js/selectores.js
+++ Cuerso_Js/AprediendoJquery/js/selectores.js
@@ -9,16 +9,29 @@ $(document).ready(function(){
                 .css("color", "white");
 
     // Selector de clases
-    var mi_clase = $('.zebra');
+    var mi_clase = $('.zebra').css("padding", "5px");
 
    //mi_clase.css("border", "5px dashed black")
     
     $('.sin_borde').click(function(){
         console.log("Click dado");
-        
         $(this).addClass('zebra');
     });
     
+
+
+    //Selectores de etiqueta
+    var parrafos = $('p').css("cursor", "pointer");
+
+    parrafos.click(function(){
+        var that = $(this);
+        if (!that.hasClass('grande')) {
+            that.addClass('grande')
+        }else{
+            that.removeClass('grande')
+        }
+        
+    });
 
 });
 // $ se da referencia a jQery despues de eseo selector posterior evento

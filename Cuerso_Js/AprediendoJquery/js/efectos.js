1107685ce12cb2baadb20ab1759c5892f8ac72fb
JuanMarin <wvany1996@gmail.com>
2018-12-24 18:41:19 -0500

    Animaciones personalizadas

--- Cuerso_Js/AprediendoJquery/js/efectos.js
+++ Cuerso_Js/AprediendoJquery/js/efectos.js
@@ -2,20 +2,42 @@
 
 $(document).ready(function(){
     
+    var caja = $("#caja");
 
     $("#mostrar").click(function () {
         $(this).hide();
         $("#ocultar").show();
-        $("#caja").fadeTo('slow',1);
+        caja.fadeTo('slow',1);
     });
 
     $("#ocultar").click(function () {
         $(this).hide();
         $("#mostrar").show();
-        $("#caja").fadeTo('slow',0);
+        caja.fadeTo('slow',0);
     });
 
     $("#todo").click(function(){
-        $("#caja").toggle('fast');
+        caja.toggle('fast');
+    });
+    $("#animame").click(function (){
+        caja.animate({
+            marginLeft: '500px',
+            fontSize: '40px',
+            height: '110px',
+            }, 'slow')
+            .animate({
+                borderRadius:'900px',
+                marginTop: '80px'
+            }, 'slow').animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            }, 'slow').animate({
+                borderRadius: '100px',
+                marginTop: '0px'
+            }, 'slow').animate({
+                marginLeft: '500px',
+                fontSize: '40px',
+                height: '110px',
+            }, 'slow')
     });
 });

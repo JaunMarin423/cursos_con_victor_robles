2accccc61f3c97d4fbe84a5c72915bc6222e8939
JuanMarin <wvany1996@gmail.com>
2019-02-03 20:42:13 -0500

    Árbol para Gatos Rascador

--- Cuerso_Js/Proyecto-js/js/main.js
+++ Cuerso_Js/Proyecto-js/js/main.js
@@ -92,4 +92,13 @@ $(document).ready(function () {
         }
 
 
+        //Scroll Ariba de la web
+        $('.subir').click(function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+
 });

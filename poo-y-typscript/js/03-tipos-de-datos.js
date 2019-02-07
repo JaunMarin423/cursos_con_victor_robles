a8609fb80a314f96d58919a5eebc3dfb5f285e9b
JuanMarin <wvany1996@gmail.com>
2019-02-07 17:38:59 -0500

    Let vs Var en TS

--- poo-y-typscript/js/03-tipos-de-datos.js
+++ poo-y-typscript/js/03-tipos-de-datos.js
@@ -11,4 +11,14 @@ cualquiera = 77;
 // Array 
 var lenguajes = ["php", "js", "css", 13];
 var years = [13, 12, 14];
+// let vs var
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    var numero1_1 = 44;
+    var numero2 = 55;
+    console.log(numero1_1, numero2);
+}
+console.log(numero1, numero2);
 console.log(cadena, edad, verdadero_falso, cualquiera, lenguajes, years);
+// Cuando utiliso let es por bloque y var es para una variable global
--- poo-y-typscript/js/03-tipos-de-datos.ts
+++ poo-y-typscript/js/03-tipos-de-datos.ts
@@ -24,4 +24,22 @@ var lenguajes: Array <any> = ["php", "js", "css", 13];
 
 let years: number[]= [13, 12, 14];
 
+// let vs var
+
+var numero1 = 10;
+var numero2 = 12;
+
+if(numero1 == 10){
+    let numero1 = 44;
+    var numero2 = 55;
+    console.log(numero1, numero2);
+    
+}
+
+console.log(numero1, numero2);
+
+
 console.log(cadena, edad, verdadero_falso, cualquiera, lenguajes, years);
+
+
+// Cuando utiliso let es por bloque y var es para una variable global

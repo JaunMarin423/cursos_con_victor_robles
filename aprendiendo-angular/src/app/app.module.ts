f004f3c44b5fa96fe76728088d8fd2d49e53a096
JuanMarin <wvany1996@gmail.com>
2019-02-13 18:29:13 -0500

    Multiples / Varios componentes

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { VideojuegoComponent } from '../videojuego/videojuego.component'
+import { VideojuegoComponent } from '../videojuego/videojuego.component';
+import { SapatosComponent } from '../app/sapatos/sapatos.components';
+
 @NgModule({
   declarations: [
     AppComponent,
-    VideojuegoComponent
+    VideojuegoComponent,
+    SapatosComponent
   ],
   imports: [
     BrowserModule
---
+++ aprendiendo-angular/src/app/sapatos/sapatos.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sapatos',
+    templateUrl: './sapatos.component.html'
+})
+
+export class SapatosComponent {
+    public titulo: string ="Sapatos cualquiera";
+
+}

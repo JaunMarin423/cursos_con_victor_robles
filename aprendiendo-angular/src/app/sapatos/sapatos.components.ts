99392dc2ff983f393a06f699e6a1a334ab9d1ba0
JuanMarin <wvany1996@gmail.com>
2019-02-17 20:51:18 -0500

    Directiva bucle ngFor

--- aprendiendo-angular/src/app/sapatos/sapatos.components.ts
+++ aprendiendo-angular/src/app/sapatos/sapatos.components.ts
@@ -9,11 +9,14 @@ import { Zapatilla } from '../models/zapatillas';
 export class SapatosComponent implements OnInit {
     public titulo: string ="Sapatos cualquiera";
     public zapatillas: Array<Zaparilla>;
+    public marcas: String[];
     
     constructor(){
+        this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Nike classic', 'Nike', 'Blanco', 40, true),
             new Zapatilla('Rebook classic', 'Rebook', 'violeta', 180, true),
+            new Zapatilla('Rebook Spartan', 'Rebook', 'violeta', 180, true),
             new Zapatilla('Adidas classic', 'adidas', 'verde', 60, false),
             new Zapatilla('Adidas Zebra', 'adidas', 'verde', 190, false)
         ];
@@ -21,6 +24,19 @@ export class SapatosComponent implements OnInit {
 
     ngOnInit(){
         console.log(this.zapatillas);
-        
+
+        this.getMarcas();
+    }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index)=>{
+
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            }
+            
+        });
+
+        console.log(this.marcas);
     }
 }

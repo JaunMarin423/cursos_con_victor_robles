ed2059cfd1ffd46e428fcc8e167c9107121c1a17
JuanMarin <wvany1996@gmail.com>
2019-02-23 23:23:11 -0500

    Configurar el routing de Angular

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 
 import { VideojuegoComponent } from '../videojuego/videojuego.component';
 import { SapatosComponent } from '../app/sapatos/sapatos.components';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     SapatosComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+// Importar modulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Importar componentes
+import { HomeComponent } from './home/home.component';
+import { SapatosComponent } from './sapatos/sapatos.components';
+import { VideojuegoComponent } from '../videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+// Array de rutas
+const appRoutes: Routes = [
+    { path: '', component: 'HomeComponent'},
+    { path: 'sapatos', component: 'SapatosComponent' },
+    { path: 'videojuego', component: 'VideojuegoComponent' },
+    { path: 'cursos', component: 'CursosComponent' },
+    { path: '**', component: HomeComponent }
+];
+
+// Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);

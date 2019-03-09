96cbe5379e055237a2815a3625e264a312747697
JuanMarin <wvany1996@gmail.com>
2019-03-09 06:32:50 -0500

    Crear servidor con Node JS

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// cargar archivos de rutas
+
+// middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// cors
+
+// rutas
+app.get('/', (req, res) => {
+    res.status(200).send(
+        "<h1>Pagina de inicio</h1>"
+    );
+});
+
+
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        message: "Hola mundo desde el api de nodeJS"
+    });
+});
+
+// exportat
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3700;
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/portafolio')
-        .then(() =>{
-            console.log("Conecxión a la base de datos establecida satisfactoriamente");
+        .then(() => {
+            console.log("Conecxión a la base de datos establecida satisfactoriamente...");
+
+            // Creacion del servidor
+            app.listen(port, () => {
+                console.log("servidor corriendo correctamente en la url:  localhost:3700");
+                
+            });
             
         })
-        .catch(err => console.log(err)
-        );
+        .catch(err => console.log(err));

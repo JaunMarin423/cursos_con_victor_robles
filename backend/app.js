d4c42e6855ff309cf7e00609cd861683c936b289
JuanMarin <wvany1996@gmail.com>
2019-03-11 11:22:23 -0500

    Como usar un cliente RESTful

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/test', (req, res) => {
+app.post('/test/:id', (req, res) => {
+    console.log(req.body.Nombre);
+    console.log(req.params.id);
+    
     res.status(200).send({
         message: "Hola mundo desde el api de nodeJS"
     });

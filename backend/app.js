7a652ecf5c8277c5d30c1368b32c0e62f4a9a035
JuanMarin <wvany1996@gmail.com>
2019-03-19 15:11:31 -0500

    Controladores y rutas en Node

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // cargar archivos de rutas
+var project_routes = require('./rutes/project');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended:false}));
@@ -14,21 +15,7 @@ app.use(bodyParser.json());
 // cors
 
 // rutas
-app.get('/', (req, res) => {
-    res.status(200).send(
-        "<h1>Pagina de inicio</h1>"
-    );
-});
-
-
-app.post('/test/:id', (req, res) => {
-    console.log(req.body.Nombre);
-    console.log(req.params.id);
-    
-    res.status(200).send({
-        message: "Hola mundo desde el api de nodeJS"
-    });
-});
+app.use('/api', project_routes);
 
 // exportat
 module.exports = app;

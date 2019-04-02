9e2b352582e220cf42d847a6e782f84b40584a67
JuanMarin <wvany1996@gmail.com>
2019-04-02 16:22:39 -0500

    Método en el backend para devolver imágenes

--- backend/rutes/project.js
+++ backend/rutes/project.js
@@ -16,5 +16,6 @@ router.get('/projects', ProjectController.getProjects);
 router.put('/project/:id', ProjectController.updateProject);
 router.delete('/project/:id', ProjectController.delateProject);
 router.post('/upload-image/:id', multipartMiddleware, ProjectController.uploadImage);
+router.get('/get-image/:image', ProjectController.getImageFile);
 
 module.exports = router;
